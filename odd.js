9659d9bdc77e6099003e4d8afa32c6ad3180aa74
Jaskarn <jaskarn15@gmail.com>
2021-10-10 04:49:52 +0000

    Using functions to determine is a number is odd

---
+++ odd.js
@@ -0,0 +1,8 @@
+const isOdd = function(num) {
+  return num % 2 !== 0;
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+ console.log(isOdd(5));
+ console.log(isOdd(10));
+ 
