46699d25d81990cf60c058a99e5fff0debc76d17
Jaskarn <jaskarn15@gmail.com>
2021-10-11 05:53:06 +0000

    Using for and while loops to print elements of an array and also print elements in reverse order

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (let t = 0; t < ingredients.length; t++) {
+  console.log(ingredients[t]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (let r = ingredients.length - 1; r >= 0; r--) {
+  console.log(ingredients[r]);
+}
